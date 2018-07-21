ce66bd4352b6d20902a6109c568046a4fd90bcc1
Vic <ngenovictor321@gmail.com>
2018-07-21 15:52:49 +0300

    add a database connection object

---
+++ app/src/main/java/com/roomwordsample/roomwordsample/WordRoomDatabase.java
@@ -0,0 +1,27 @@
+package com.roomwordsample.roomwordsample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase{
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    public static WordRoomDatabase getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (WordRoomDatabase.class){
+                if (INSTANCE == null){
+                    // create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
