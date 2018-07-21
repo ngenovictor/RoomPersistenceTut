d45b5ec121affc2b242e411da72b3d7b5d25c565
Vic <ngenovictor321@gmail.com>
2018-07-21 16:27:03 +0300

    add a repository to act as a communicating link between the Network and database

---
+++ app/src/main/java/com/roomwordsample/roomwordsample/WordRepository.java
@@ -0,0 +1,40 @@
+package com.roomwordsample.roomwordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application){
+        WordRoomDatabase database = WordRoomDatabase.getDatabase(application);
+        mWordDao = database.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords(){
+        return mAllWords;
+    }
+
+    public void insert(Word word){
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void>{
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao wordDao){
+            mAsyncTaskDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... words) {
+            mAsyncTaskDao.insert(words[0]);
+            return null;
+        }
+    }
+}
